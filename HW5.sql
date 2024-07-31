420b521f80f9112011b59acf6232f7130cca2fc6
serhatky <126700423+serhatky@users.noreply.github.com>
2024-07-31 23:07:55 +0300

    Add files via upload

---
+++ HW5.sql
@@ -0,0 +1,17 @@
+
+--SENARYO1
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+LIMIT 5;
+--SENARYO2
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length 
+OFFSET 5
+LIMIT 5;
+--SENARYO3
+SELECT * FROM customer
+WHERE store_id=1
+ORDER BY last_name DESC
+LIMIT 4;
